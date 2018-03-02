550e54b381c4dfdd92c4fa4b93fa6242c632793a
tetiana.dmitrenko@skynix.co <tetiana.dmitrenko@skynix.co>
2018-03-02 16:36:12 +0200

    rebuild, added services

---
+++ front-end/src/app/admin/routes/dashboard/users/index/index.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {NgDataJsonserviceService} from '../../../../services/ng-data-jsonservice.service';
+import {ActivatedRoute} from '@angular/router';
+import {UserServiceService} from "../../../../services/user/user-service.service";
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.sass']
+})
+export class UsersIndexComponent implements OnInit {
+  data: any;
+  outputData: any;
+  columns: any = ['NAME', 'EMAIL'];
+  constructor(private getDataService: NgDataJsonserviceService, private userService: UserServiceService) { }
+
+  changeData(data) {
+    this.outputData = data;
+  }
+
+  ngOnInit() {
+    // this.userService.fetch().subscribe(res => {
+    //   console.log('all users', res);
+    //   this.outputData = res;
+    // });
+    this.getDataService.getData('users.json').subscribe(res => {
+      this.outputData = res;
+    });
+  }
+
+}
---
+++ front-end/src/app/admin/routes/dashboard/users/users.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {NgDataJsonserviceService} from '../../../services/ng-data-jsonservice.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  data: any;
+  outputData: any;
+  columns: any = ['NAME', 'EMAIL'];
+  constructor(private getDataService: NgDataJsonserviceService, private route: ActivatedRoute) { }
+
+  changeData(data) {
+    this.outputData = data;
+  }
+
+  ngOnInit() {
+    this.getDataService.getData('users.json').subscribe(res => {
+      this.data = res.map(e => {
+        return {
+          id: e.id,
+          data: {
+            name: e.name,
+            email: e.email
+          },
+          photo: e.photo
+        };
+      });
+      this.outputData = this.data;
+    });
+    this.route.queryParams.subscribe(params => {
+      console.log(params);
+    });
+  }
+
+}
